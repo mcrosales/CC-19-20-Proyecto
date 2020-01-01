0937dbca9e9eaf2095cf565ac0f8bbd43612c47c
mcrosales <mariocrosl@gmail.com>
2020-01-02 00:30:22 +0100

    vendors_products microservice

---
+++ vendors_products/src/main/java/ugr/cc/vendors_products/VendorsProductsApplication.java
@@ -0,0 +1,13 @@
+package ugr.cc.vendors_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorsProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorsProductsApplication.class, args);
+	}
+
+}
---
+++ vendors_products/src/test/java/ugr/cc/vendors_products/VendorsProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package ugr.cc.vendors_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorsProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
